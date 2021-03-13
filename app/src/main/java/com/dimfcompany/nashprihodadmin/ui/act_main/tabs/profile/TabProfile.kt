8196c4a3314ce312ef3548e362c3183afc35a947
bios90 <bios90@mail.ru>
2021-03-13 22:32:27 +0300

    *** Added test user ***

--- app/src/main/java/com/dimfcompany/nashprihodadmin/ui/act_main/tabs/profile/TabProfile.kt
+++ app/src/main/java/com/dimfcompany/nashprihodadmin/ui/act_main/tabs/profile/TabProfile.kt
@@ -1,6 +1,7 @@
 package com.dimfcompany.nashprihodadmin.ui.act_main.tabs.profile
 
 import android.view.View
+import com.dimfcompany.nashprihodadmin.logic.models.ModelUser
 import com.dimfcompany.nashprihodadmin.ui.act_main.ActMain
 import com.dimfcompany.nashprihodadmin.ui.act_main.tabs.TabPresenter
 
@@ -12,6 +13,7 @@ class TabProfile(act_main: ActMain) : TabPresenter
     {
         mvp_view = act_main.view_factory.getLaProfileMvpView(null)
 
+        mvp_view.bindUser(ModelUser.getTestUser())
     }
 
     override fun getView(): View
